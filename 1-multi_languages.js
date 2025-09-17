48c2834413622ea5ac099d2af78cd50ee7b0725f
BELINDAH120 <128031203+BELINDAH120@users.noreply.github.com>
2025-09-17 05:45:10 +0100

    Update 1-multi_languages.js
    Changed the code

--- 1-multi_languages.js
+++ 1-multi_languages.js
@@ -1,4 +1,10 @@
-const myVar = "C is fun";
-const myVar = "Python is cool";
-const myVar = "JavaScript is amazing";
-console.log(myVar);
+#!/usr/bin/node
+
+const line1 = 'C is fun';
+const line2 = 'Python is cool';
+const line3 = 'JavaScript is amazing';
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
+
